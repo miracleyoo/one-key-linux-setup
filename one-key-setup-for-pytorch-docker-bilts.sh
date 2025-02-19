2283dc309d553d710a53cdd0d2c9175f4b4f29c0
Miracleyoo <786671043@qq.com>
2025-02-19 10:46:37 -0600

    Update one-key-setup-for-pytorch-docker-bilts.sh

--- one-key-setup-for-pytorch-docker-bilts.sh
+++ one-key-setup-for-pytorch-docker-bilts.sh
@@ -75,6 +75,15 @@ conda install -y conda-forge/label/cf202003::blosc-hdf5-plugin=1.0.0
 imageio_download_bin freeimage
 # conda install -y freeimage -c conda-forge
 
+# Install hdf5-blosc
+git clone https://github.com/Blosc/hdf5-blosc
+cd hdf5-blosc
+mkdir build
+cd build
+cmake ..
+make
+make install
+
 
 
 # conda create -y -n bilts python=3.11 pip
